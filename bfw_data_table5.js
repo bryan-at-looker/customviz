caa0a7e41e0c1eab427da767365472b05fcbeb42
Bryan Weber <Looker@Bryan-Webers-MacBook-Air.local>
2018-04-22 09:38:57 -0700

    yo

--- bfw_data_table5.js
+++ bfw_data_table5.js
@@ -6,16 +6,31 @@ looker.plugins.visualizations.add({
   },
   update: function (data, element, config, queryResponse, details) {
 
-    var html = "";
+    var html = '<table id="example" style="width:100%">';
+
+    html += '<thead><tr>';
+
+    html += '<th>Close Date</th>';
+
+    html += '</tr></thead>'
+
+    html += '<tbody>';
+
     for(var row of data) {
         var cell = row[queryResponse.fields.dimensions[0].name];
+        html += '<tr><td>'
         html += LookerCharts.Utils.htmlForCell(cell);
+        html += '</td></tr>'
     }
+    
+    var html += '</tbody>';
+
+    var html += '</table>';
+
     element.innerHTML = html;
 
-    // element.innerHTML = html;
-    // $(document).ready(function() {
-    //   $('#example').DataTable();
-    // } );
+    $(document).ready(function() {
+      $('#example').DataTable();
+    } );
   }
 });
