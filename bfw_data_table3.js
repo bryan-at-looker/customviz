eff71301c13b727d89a4877d4d57e2f00016cd72
Bryan Weber <Looker@Bryan-Webers-MacBook-Air.local>
2018-04-22 08:19:57 -0700

    yo

---
+++ bfw_data_table3.js
@@ -0,0 +1,16 @@
+looker.plugins.visualizations.add({
+    id: "demo_viz",
+    label: "Demo",
+    create: function (element, config) {
+        element.innerHTML = "<p></p>";
+    },
+    update: function (data, element, config, queryResponse, details) {
+        var html = '<img style="width: 50%; height: 50%" src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?cs=srgb&dl=animal-animal-photography-cat-104827.jpg&fm=jpg"/></br><table id="table_id" class="display"></table>';
+        element.innerHTML = html;
+        $(document).ready( function () {
+             $('#table_id').DataTable();
+        } );
+
+} );;
+    }
+});
