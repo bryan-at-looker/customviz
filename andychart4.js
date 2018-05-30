67dd42378518c8eb5ed0a488a993e8adf1b6be91
bryan-at-looker <bryan-at-looker@users.noreply.github.com>
2018-05-30 16:54:33 -0700

    Update and rename andychart3.js to andychart4.js

--- andychart4.js
+++ andychart4.js
@@ -22,7 +22,11 @@
           update: function(data, element, config, queryResponse) {
 
 Highcharts.chart('andychart', {
-    xAxis: {
+  chart: {
+        animation: {
+            duration: 10000
+  },
+  xAxis: {
         categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
 
         plotLines: [{
