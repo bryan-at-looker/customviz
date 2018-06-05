6978f03ad3d4afe4495e6fb7900ff6a126666aeb
bryan-at-looker <bryan-at-looker@users.noreply.github.com>
2018-06-05 07:46:18 -0700

    Create test_table.js

---
+++ test_table.js
@@ -0,0 +1,71 @@
+  (function() {
+        var viz = {
+          id: "500friends",
+          label: "TEST - 500friends Table",
+          options: {
+            previous_month: {
+              section: "Chart",
+              type: "boolean",
+              label: "Turn on Previous Month",
+              display: "radio",
+              default: false
+            }
+          },
+
+          // Set up the initial state of the visualization
+          create: function(element, config) {
+              var css = element.innerHTML = `
+              <div id="500buddies"></div>
+              `;
+          },
+          // Render in response to the data or settings changing
+          update: function(data, element, config, queryResponse) {
+
+            if ($("#500buddies").hasClass("tabulator")){ $("#500buddies").tabulator("destroy") }
+
+            var clmns = [ { title: "Measure", field: "measure", sortable: true, datanum:-1 } ];
+            for (var i=0; i<data.length; i++) {
+              var temp =  { title: data[i][queryResponse.fields.dimension_like[0].name].value,
+                            field: data[i][queryResponse.fields.dimension_like[0].name].value,
+                            datanum: i,
+                            sortable:true
+                          };
+              clmns.push(temp)
+            }
+
+            var rws = []
+            for (var i=0; i<queryResponse.fields.measure_like.length; i++) {  
+              var rw = {measure: queryResponse.fields.measure_like[i].label_short};
+              for (j=1; j<clmns.length; j++) {
+                rw[clmns[j].field] = data[clmns[j].datanum][queryResponse.fields.measure_like[i].name].value
+              }
+              rws.push(rw);
+            }
+
+
+            if (config.previous_month) {
+              clmns.push({ title: "Over Previous Month", field: "previous_month", sortable: true, datanum:-2, 
+                            formatter:function(cell, formatterParams){
+                              let val = (Math.round(cell.getValue()*100));
+                              val = (val<0) ? '<font color="red">▼ '+Math.abs(val).toFixed(2)+'%</font>' : '<font color="green">▲ '+ val.toFixed(2)+'%</font>'
+                              return val
+                            }
+              });
+
+              $.each( rws, function(i, val) {
+                  rws[i]['previous_month'] = (1.0 * val[data[0][queryResponse.fields.dimension_like[0].name].value] / val[data[1][queryResponse.fields.dimension_like[0].name].value] - 1 );
+              });
+            }
+
+
+
+            var tbl = $("#500buddies").tabulator({
+              layout: "fitColumns",
+              columns: clmns,
+              data: rws
+            });
+
+          }
+      };
+      looker.plugins.visualizations.add(viz);
+      }());
